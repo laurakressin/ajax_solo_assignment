64dbbddecb1a7b9b7e605aacb151cbe1b7a5e662
Laura Kressin <laurakressin@gmail.com>
2015-09-20 16:46:19 -0500

    submit works, one append instead of 3

--- examples.js
+++ examples.js
@@ -5,30 +5,30 @@ function searchCallback(results) {
     console.log(results);
 }
 
-// var searchQuery ;
+var searchQuery = "";
 
 $(document).ready(function(){
     $(".submit").on('click', function(){
-        var searchQuery = $('.movieName').val();
+        event.preventDefault();
+        searchQuery = $('.movieName').val();
         console.log("The Movie " + searchQuery);
         search(searchQuery);
     });
-
 
 });
 
 function search(query){
     $.ajax({
-    	url:"http://www.omdbapi.com/?t="+(query).replace(' ', '+')+"&y=&plot=short&r=json"
+    	url:"http://www.omdbapi.com/?t="+encodeURI(query)+"&y=&plot=short&r=json"
     }).done(function(movie){
         searchCallback(movie);
         movieData = movie; 
         var $movieTitle = ("<p>"+movie.Title+"</p>");
         var $movieYear = ("<p>"+movie.Year+"</p>");
         var $moviePlot = ("<p>"+movie.Plot+"</p>");
-        $(".movieTitle").append($movieTitle);
-        $(".movieYear").append($movieYear);
-        $(".moviePlot").append($moviePlot);
+        $(".searchResultDisplay").append($movieTitle + "<br>" + $movieYear + "<br>" + $moviePlot);
+        // $(".movieYear").append($movieYear);
+        // $(".moviePlot").append($moviePlot);
         // searchCallback(data.results);
         // function query() {
         // var input = document.getElementById("userInput").value;
