a8c72fec1ce9355f5c23cea6ecd85ea9c61a573a
Laura Kressin <laurakressin@gmail.com>
2015-09-20 14:49:53 -0500

    search bar and url functionality

--- examples.js
+++ examples.js
@@ -1,13 +1,41 @@
+// var output = ('input').Replace(" ", "+");
+var movieData;
 
-$(document).ready(function() {
+function searchCallback(results) {
+    console.log(results);
+}
 
-    $.ajax('data.json', {
-        success: function(response){
-            console.log(response);
-        },
-        error: function(request, errorType, errorMessage){
-            alert(errorType + " " + errorMessage);
-        }
+// var searchQuery ;
+
+$(document).ready(function(){
+    $(".submit").on('click', function(){
+        var searchQuery = $('.movieName').val();
+        console.log("The Movie " + searchQuery);
+        search(searchQuery);
     });
 
-})
+
+});
+
+function search(query){
+    $.ajax({
+    	url:"http://www.omdbapi.com/?t="+(query).replace(' ', '+')+"&y=&plot=short&r=json"
+    }).done(function(movie){
+        searchCallback(movie);
+        movieData = movie; 
+        var $movieTitle = ("<p>"+movie.Title+"</p>");
+        var $movieYear = ("<p>"+movie.Year+"</p>");
+        var $moviePlot = ("<p>"+movie.Plot+"</p>");
+        $(".movieTitle").append($movieTitle);
+        $(".movieYear").append($movieYear);
+        $(".moviePlot").append($moviePlot);
+        // searchCallback(data.results);
+        // function query() {
+        // var input = document.getElementById("userInput").value;
+        // alert(input);
+        // }
+    });
+// }
+};
+
+
